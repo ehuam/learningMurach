c7ce03c89a29836d2003f785472188bfd4916776
edgar@huamantla <edgar@huamantla.biz>
2019-01-23 20:09:39 -0500

    function: read records from a csv and place them into a list

---
+++ source/book_excercises/ch 16/db.py
@@ -0,0 +1,13 @@
+import csv
+from business import Product
+
+FILENAME = 'products.csv'
+
+def get_products():
+    products = []
+    with open(FILENAME, newline='') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            product = Product(row[0], float(row[1]), int(row[2]))
+            products.append(product)
+        return products
